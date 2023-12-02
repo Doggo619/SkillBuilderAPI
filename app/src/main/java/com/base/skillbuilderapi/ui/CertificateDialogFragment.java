57c399f611bfd2938738433ce3306f0bf2203c50
Doggo619 <parik101198@gmail.com>
2023-12-02 14:44:55 +0530

    color certificates

--- app/src/main/java/com/base/skillbuilderapi/ui/CertificateDialogFragment.java
+++ app/src/main/java/com/base/skillbuilderapi/ui/CertificateDialogFragment.java
@@ -24,9 +24,10 @@ import com.squareup.picasso.Picasso;
 import java.util.Date;
 
 public class CertificateDialogFragment extends DialogFragment {
-    MaterialTextView userName, elementName, chapterName, date;
-    ImageView profilePicture;
+    MaterialTextView userName, elementName, chapterName, date, congratsMessage, certificateName, certificateOfDash, successfully, andCompleted, skillBuilder;
+    ImageView profilePicture, border, certificateLogo, mainCertificateLogo, star;
     ImageFilterView closeIcon;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -40,6 +41,16 @@ public class CertificateDialogFragment extends DialogFragment {
         date = view.findViewById(R.id.tvDate);
         profilePicture = view.findViewById(R.id.ivProfilePicture);
         closeIcon = view.findViewById(R.id.ivClose);
+        border = view.findViewById(R.id.ivBorderImage);
+        certificateLogo = view.findViewById(R.id.ivCertificateLogo);
+        mainCertificateLogo = view.findViewById(R.id.ivMainCertificateLogo);
+        congratsMessage = view.findViewById(R.id.tvCongratsMessage);
+        certificateName = view.findViewById(R.id.tvCertificate);
+        certificateOfDash = view.findViewById(R.id.tvCertificateOfCompletion);
+        star = view.findViewById(R.id.ivStar);
+        successfully = view.findViewById(R.id.tvBody);
+        andCompleted = view.findViewById(R.id.tvAndCompleted);
+        skillBuilder = view.findViewById(R.id.tvCompleted);
 
         closeIcon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,13 +72,95 @@ public class CertificateDialogFragment extends DialogFragment {
             String chapterNameStr = args.getString("chapterName");
             ElementProgressList elementProgress = ElementProgressList.fromJson(elementProgressStr);
 
+            int maxStar = elementProgress.getMaxStar();
+
+            if (elementProgress.getCertificateEarned() == 4001) {
+                border.setImageResource(R.drawable.certificate_green);
+                certificateLogo.setImageResource(R.drawable.ic_certificate_icon_green);
+                mainCertificateLogo.setImageResource(R.drawable.ic_certificate_icon_green);
+                congratsMessage.setText(getResources().getText(R.string.certificate_congrats_message_with_vowels_proactiveLearner));
+                certificateName.setText(getResources().getText(R.string.certificate_of_achievement_proactiveLearner));
+                certificateName.setTextColor(getResources().getColor(R.color.green));
+                certificateOfDash.setText(getResources().getText(R.string.certificate_of_completion));
+                star.setVisibility(View.GONE);
+                userName.setTextColor(getResources().getColor(R.color.green));
+                successfully.setText(getResources().getText(R.string.successfully_completed));
+                andCompleted.setVisibility(View.GONE);
+                skillBuilder.setText(getResources().getString(R.string.current_progress, elementProgress.getCurrentProgress()));
+            } else if (elementProgress.getCertificateEarned() == 4002) {
+                border.setImageResource(R.drawable.certificate_orange);
+                certificateLogo.setImageResource(R.drawable.ic_certificate_icon_orange);
+                mainCertificateLogo.setImageResource(R.drawable.ic_certificate_icon_orange);
+                congratsMessage.setText(getResources().getText(R.string.certificate_congrats_message_with_vowels_goodPerformer));
+                certificateName.setText(getResources().getText(R.string.certificate_of_achievement_goodPerformer));
+                certificateName.setTextColor(getResources().getColor(R.color.orange));
+                certificateOfDash.setText(getResources().getText(R.string.certificate_of_achievement));
+                if (elementProgress.getSbType() > 1) {
+                    star.setVisibility(View.VISIBLE);
+                    switch (maxStar) {
+                        case 0:
+                            star.setImageResource(R.drawable.zerostar);
+                            break;
+                        case 1:
+                            star.setImageResource(R.drawable.onestar);
+                            break;
+                        case 2:
+                            star.setImageResource(R.drawable.twostar);
+                            break;
+                        case 3:
+                            star.setImageResource(R.drawable.threestar);
+                            break;
+                        default:
+                            star.setImageResource(R.drawable.ic_baseline_share_24);
+                            break;
+                    }
+                }
+                userName.setTextColor(getResources().getColor(R.color.orange));
+                successfully.setText(getResources().getString(R.string.successfully_won, maxStar));
+                andCompleted.setVisibility(View.VISIBLE);
+                skillBuilder.setText(getResources().getString(R.string.current_progress, elementProgress.getCurrentProgress()));
+            } else {
+                border.setImageResource(R.drawable.certificate_blue);
+                certificateLogo.setImageResource(R.drawable.ic_certificate_icon_blue);
+                mainCertificateLogo.setImageResource(R.drawable.ic_certificate_icon_blue);
+                congratsMessage.setText(getResources().getText(R.string.certificate_congrats_message_with_vowels_starStudent));
+                certificateName.setText(getResources().getText(R.string.certificate_of_achievement_starStudent));
+                certificateName.setTextColor(getResources().getColor(R.color.blue));
+                certificateOfDash.setText(getResources().getText(R.string.certificate_of_achievement));
+                if (elementProgress.getSbType() > 1) {
+                    star.setVisibility(View.VISIBLE);
+                    switch (maxStar) {
+                        case 0:
+                            star.setImageResource(R.drawable.zerostar);
+                            break;
+                        case 1:
+                            star.setImageResource(R.drawable.onestar);
+                            break;
+                        case 2:
+                            star.setImageResource(R.drawable.twostar);
+                            break;
+                        case 3:
+                            star.setImageResource(R.drawable.threestar);
+                            break;
+                        default:
+                            star.setImageResource(R.drawable.ic_baseline_share_24);
+                            break;
+                    }
+                }
+                userName.setTextColor(getResources().getColor(R.color.blue));
+                successfully.setText(getResources().getString(R.string.successfully_won, maxStar));
+                andCompleted.setVisibility(View.VISIBLE);
+                skillBuilder.setText(getResources().getString(R.string.current_progress, elementProgress.getCurrentProgress()));
+            }
+
 
             long dateFormat = elementProgress.getCertificateDate();
+
             Date date1 = new Date(dateFormat * 1000);
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
                 SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
-                String formatedDate = sdf.format(date1);
-                date.setText(formatedDate);
+                String formattedDate = sdf.format(date1);
+                date.setText(formattedDate);
             }
             chapterName.setText(chapterNameStr);
             userName.setText(elementProgress.getUserName());
